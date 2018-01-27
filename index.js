d198a104e960e01ce48f465db325ffdae1b9eb69
pekoe09 <juha.kangas@welho.com>
2018-01-27 11:26:17 +0200

    Tehtävä 3.5

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
   {
@@ -32,14 +35,22 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.post('/api/persons', (req, res) => {
+  const person = req.body
+  const id = getRandomId(4, 1000000000)
+  person.id = id
+  persons = persons.concat(person)
+  res.json(person)
+})
+
 app.get('/api/persons/:id', (req, res) => {
   let match = persons.find(person =>
     person.id === Number(req.params.id))
-    if(match) {
-      res.json(match)
-    } else {
-      res.status(404).end()
-    }
+  if (match) {
+    res.json(match)
+  } else {
+    res.status(404).end()
+  }
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -51,3 +62,9 @@ const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+getRandomId = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
+}
