72cecc0f25be256db3d48817e63238e0583dd467
Juha <juha.kangas@welho.com>
2018-01-28 19:15:59 +0200

    Tehtävä 3.16

--- index.js
+++ index.js
@@ -107,8 +107,14 @@ app.put('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  persons = persons.filter(person => person.id !== Number(req.params.id))
-  res.status(204).end()
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const PORT = process.env.PORT || 3001
